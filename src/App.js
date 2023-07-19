ebce70968c6a598081e7e22f0ed08b5353cd0e91
dragonyears <dragonyears@hanil.com>
2023-07-19 09:35:48 +0900

    project init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+// import ChildComponent from "./child";
+// import TimerComponent from "./timer";
+import MainPage from "./main";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // const text = "인프런 수강생 여러분 화이팅!";
+  // const sayHello = function () {
+  //   return <h3>인프런 강의 너무 좋아</h3>;
+  // };
+  // const sayHello2 = function () {
+  //   alert("안녕하세요");
+  // };
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //     <div>
+  //       <h1>안녕하세요!</h1>
+  //       <h2>{text}</h2>
+  //       {sayHello()}
+  //       <div onClick={sayHello2}>클릭해주세요</div>
+  //       <TimerComponent />
+  //       <ChildComponent name="그랩" age={27} />
+  //       <ChildComponent name="민수" age={25} />
+  //       <ChildComponent name="철수" age={30} />
+  //     </div>
+  //   </div>
+  // );
+  return <MainPage />;
 }
 
 export default App;
---
+++ src/main/index.js
@@ -0,0 +1,60 @@
+import React from "react";
+import "./index.css";
+import axios from "axios";
+function MainPage() {
+  const [products, setProducts] = React.useState([]);
+  React.useEffect(function () {
+    axios
+      .get(
+        "https://a71bf8fe-e94f-4461-a3a9-f728ff6b39db.mock.pstmn.io/products"
+      )
+      .then(function (result) {
+        const products = result.data.products;
+        setProducts(products);
+      })
+      .catch(function (error) {
+        console.error("에러 발생 : ", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <div id="header">
+        <div id="header-area">
+          <img src="images/icons/logo.png" />
+        </div>
+      </div>
+      <div id="body">
+        <div id="banner">
+          <img src="images/banners/banner1.png" />
+        </div>
+        <h1>판매되는 상품들</h1>
+        <div id="product-list">
+          {products.map(function (product, index) {
+            return (
+              <div className="product-card">
+                <div>
+                  <img className="product-img" src={product.imageUrl} />
+                </div>
+                <div className="products-contents">
+                  <span className="product-name">{product.name}</span>
+                  <span className="product-price">{product.price}원</span>
+                  <div className="product-seller">
+                    <img
+                      className="product-avatar"
+                      src="images/icons/avatar.png"
+                    />
+                    <span>{product.seller}</span>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div id="footer"></div>
+    </div>
+  );
+}
+
+export default MainPage;
